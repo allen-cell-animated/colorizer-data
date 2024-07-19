14fa71b065fa551db2680ad2c5bdc485edcdc570
Peyton Lee <peyton.lee@alleninstitute.org>
2024-07-19 16:28:30 -0700

    fix: Added additional checks for numpy values

--- colorizer_data/utils.py
+++ colorizer_data/utils.py
@@ -7,6 +7,7 @@ import pathlib
 import platform
 import re
 import shutil
+from types import NoneType
 from typing import Dict, List, Optional, Sequence, TypeVar, Union, Tuple
 
 import numpy as np
@@ -47,10 +48,20 @@ class NumpyValuesEncoder(json.JSONEncoder):
     """Handles float32 and int64 values."""
 
     def default(self, obj):
-        if isinstance(obj, np.float32):
+        if (
+            isinstance(obj, np.float32)
+            or isinstance(obj, np.double)
+            or isinstance(obj, np.float64)
+        ):
             return float(obj)
-        elif isinstance(obj, np.int64):
+        elif (
+            isinstance(obj, np.int16)
+            or isinstance(obj, np.int32)
+            or isinstance(obj, np.int64)
+        ):
             return int(obj)
+        elif isinstance(obj, NoneType):
+            return None
         return json.JSONEncoder.default(self, obj)
 
 
