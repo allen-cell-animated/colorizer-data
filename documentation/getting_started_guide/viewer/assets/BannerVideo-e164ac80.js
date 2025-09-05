3af90ebf64ecc8ffbfc7c0b232dff98411415535
Peyton Lee <peyton.lee@alleninstitute.org>
2025-09-05 16:08:45 -0700

    refactor: Moved and updated built TFE viewer
    (cherry picked from commit b652af77039a435a646e1430e526f05c3cbe58d4)

--- documentation/getting_started_guide/viewer/assets/BannerVideo-e164ac80.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./main-b5ce02e5.js";import"./gh_routing-cdb305c8.js";const r="/viewer/assets/banner_video-783a41be.mp4";function n(){return e.jsx("video",{autoPlay:!0,loop:!0,muted:!0,children:e.jsx("source",{src:r,type:"video/mp4"})})}export{n as default};
