78c37a69507fcc9a54119cfe8675fb145f30f0ab
Peyton Lee <peyton.lee@alleninstitute.org>
2025-12-17 16:58:07 -0800

    Bump version: 1.7.0 → 1.7.1

--- colorizer_data/types.py
+++ colorizer_data/types.py
@@ -8,7 +8,7 @@ from typing import Dict, List, Optional, Type, TypeVar, TypedDict, Union
 Json = Union[dict, str, int, float, bool, None]
 
 
-CURRENT_VERSION = "v1.7.0"
+CURRENT_VERSION = "v1.7.1"
 DEFAULT_COLLECTION_VERSION = "v1.0"
 DEFAULT_DATASET_VERSION = "v1.0"
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
@@ -209,7 +209,7 @@ class ColorizerMetadata(DataClassJsonMixin):
     is rewritten. Starts at 0.
     """
     _writer_version: Optional[str] = CURRENT_VERSION
-    """Version of the data writer utility scripts. Uses semantic versioning (e.g. v1.7.0)"""
+    """Version of the data writer utility scripts. Uses semantic versioning (e.g. v1.7.1)"""
 
     # Exclude these three fields from auto-encode/decode, because they need to be structured
     # together under the frameDims subfield and not as their own root-level fields.
@@ -326,7 +326,7 @@ class CollectionMetadata(DataClassJsonMixin):
     is rewritten, starting at 0.
     """
     _writer_version: Optional[str] = CURRENT_VERSION
-    """Version of the data writer utility scripts. Uses semantic versioning (e.g. v1.7.0)"""
+    """Version of the data writer utility scripts. Uses semantic versioning (e.g. v1.7.1)"""
 
 
 class CollectionDatasetEntry(TypedDict):
